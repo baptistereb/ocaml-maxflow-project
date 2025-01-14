6f4ee77e9e29a2941d638462fa184d18a3758791
Leandro-rdz <leandro.rdz604@gmail.com>
2025-01-14 19:06:29 +0100

    feat : add feature for displaying lists of results

--- src/sportgraph.ml
+++ src/sportgraph.ml
@@ -1,5 +1,4 @@
 open Graph
-(* entrée 2 Listes : Liste1 [int id,String nom, int list [voeux1,voeux2,voeux3]] , Liste2 [int id,String voeux, int capacité] *)
 
 let build_graph preferences capacities =
   let graph = empty_graph in
@@ -15,7 +14,56 @@ let build_graph preferences capacities =
   let graph = List.fold_left (fun graph (wish_id, _, capacity) -> new_arc graph {src = wish_id; tgt = sink_id; lbl = capacity }) graph  capacities in
   (*arc between strudent and wishes*)
   let graph = List.fold_left (fun graph (student_id, _, wishes) ->
-    List.fold_left (fun graph wish_id -> new_arc graph {src = student_id; tgt = wish_id; lbl = 1}) graph wishes
-  ) graph preferences in
-
+      List.fold_left (fun graph wish_id -> new_arc graph {src = student_id; tgt = wish_id; lbl = 1}) graph wishes
+    ) graph preferences in
   graph
+
+
+let results_to_list_peoples graphfinal preferences capacities = 
+  let rec aux graphfinal preferences capacities = 
+    (*Parcours la liste des gens et créé une liste d'association personne-sport*)
+    match preferences with
+    | [] -> []
+    | (student_id, student_name, wishes)::rest -> 
+      let wish_id = List.fold_left (fun acu wish_id -> 
+          let arc = find_arc graphfinal student_id wish_id in
+          match arc with
+          | None -> acu
+          | Some arc -> if arc.lbl = 1 then wish_id else acu
+        ) (-1) wishes in
+      let wish_name = List.fold_left (fun acu (id, name, _) -> if id = wish_id then name else acu) "No match found" capacities in
+      (student_name, wish_name)::(aux graphfinal rest capacities)
+  in aux graphfinal preferences capacities
+
+let results_to_list_sport graphfinal preferences capacities= 
+  let rec aux graphfinal preferences capacities = 
+    (*Parcours la liste des sport et créé une liste d'association sport-[Liste personnes]*)
+    match capacities with
+    | [] -> []
+    | (wish_id, wish_name, _)::rest -> 
+      let students = List.fold_left (fun acu (student_id, student_name, _) -> 
+          let arc = find_arc graphfinal student_id wish_id in
+          match arc with
+          | None -> acu
+          | Some arc -> if arc.lbl = 1 then (student_name)::acu else acu
+        ) [] preferences in
+      (wish_name, students)::(aux graphfinal preferences rest)
+  in aux graphfinal preferences capacities
+
+
+
+let debug_print_list_peoples l = 
+  Printf.printf "Liste des résultats :\n";
+  List.iter (fun (name, wish) -> Printf.printf "La personne %s est affecté au voeux %s\n" name wish) l
+
+let debug_print_list_sport l =
+  Printf.printf "Liste des résultats :\n";
+  List.iter (fun (wish, students) -> Printf.printf "Le voeux %s est affecté aux personnes %s\n" wish (String.concat ", " students)) l
+
+let export_all_to_txt path list_peoples list_sports = 
+  let ff = open_out path in
+  List.iter (fun (name, wish) -> Printf.fprintf ff "La personne %s est affecté au voeux %s\n" name wish) list_peoples;
+  Printf.fprintf ff "\n\n";
+  List.iter (fun (wish, students) -> Printf.fprintf ff "Le voeux %s est affecté aux personnes %s\n" wish (String.concat ", " students)) list_sports;
+  close_out ff
+
--- src/sportgraph.mli
+++ src/sportgraph.mli
@@ -3,3 +3,18 @@ open Graph
 (* entrée 2 Listes : Liste1 [int id,String nom, int list [voeux1,voeux2,voeux3]] , Liste2 [int id,String voeux, int capacité] *)
 (* build graph from parsed data *)
 val build_graph: (int * string * int list) list -> (int * string * int) list -> int graph
+
+(*return the list of (people, affected wish)*)
+val results_to_list_peoples: int graph -> (int * string * int list) list -> (int * string * int) list -> (string * string) list
+
+(*print the list above*)
+val debug_print_list_peoples: (string * string) list -> unit
+
+(*return the list of (sport, list of people affected in)*)
+val results_to_list_sport: int graph -> (int * string * int list) list -> (int * string * int) list -> (string * string list) list
+
+(*print the list above*)
+val debug_print_list_sport: (string * string list) list -> unit
+
+(*export the lists into a txt*)
+val export_all_to_txt: string -> (string * string) list -> (string * string list) list -> unit
