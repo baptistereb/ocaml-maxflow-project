da0ce44867c9e9cf955a36ca45d260ca11eb3599
Leandro-rdz <leandro.rdz604@gmail.com>
2025-01-14 19:06:43 +0100

    feat : update main

--- src/ftest.ml
+++ src/ftest.ml
@@ -1,6 +1,9 @@
 open Gfile
 open Tools
 open Findpath
+open Sportgraph
+open Parsing_preferences
+
 let () =
 
   (* Check the number of command-line arguments *)
@@ -18,31 +21,49 @@ let () =
 
 
   (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
-  
+
   let infile = Sys.argv.(1)
   and outfile = Sys.argv.(4)
   and outfile2 = Sys.argv.(5)
   and final = Sys.argv.(6)
-  
+  (*
   (* These command-line arguments are not used for the moment. *)
   and source = int_of_string Sys.argv.(2)
   and sink = int_of_string Sys.argv.(3)
+  *)
   in
 
   (* Open file *)
-  let graph_depart = from_file infile in
-  let () =export outfile2 graph_depart in 
-  let graph_depart = gmap graph_depart (fun x -> (int_of_string x)) in
-  (*let () = export outfile graph in *)
-  let graph =graph_depart in
-  (*let id_list =path_to_list (find_path graph 0 5 (fun arc -> arc.lbl > 0)) in
-  let min = take_min graph id_list max_int in
-  let graph = update_graph graph id_list min in*)
-  (*List.iter (fun x -> Printf.printf "%d " x) id_list; Printf.printf "\n\n%d " min*)
-  let (fl,fgraph)= construction_gap_solution graph source sink 0 in
-  let graph = construct_flow_solution graph_depart fgraph  in
+  (*let graph_depart = from_file infile in
+    (*let () =export outfile2 graph_depart in*) 
+    let graph_depart = gmap graph_depart (fun x -> (int_of_string x)) in
+    (*let () = export outfile graph in *)
+    let graph =graph_depart in
+    (*let id_list =path_to_list (find_path graph 0 5 (fun arc -> arc.lbl > 0)) in
+    let min = take_min graph id_list max_int in
+    let graph = update_graph graph id_list min in*)
+    (*List.iter (fun x -> Printf.printf "%d " x) id_list; Printf.printf "\n\n%d " min*)
+    let (_,fgraph)= construction_gap_solution graph source sink 0 in
+    let graph = construct_flow_solution graph_depart fgraph  in
+    let graph = gmap graph (fun x -> (string_of_int x)) in
+    let fgraph = gmap fgraph (fun x -> (string_of_int x)) in
+    let () =export outfile fgraph in 
+    let () =export final graph in 
+  *)
+
+  (*test de sportgraph*)
+  (* Liste des préférences : (id_personne, nom_personne, [id_sport1; id_sport2; id_sport3]) *)
+  let (preferences,capacities) = from_preferences_file infile in 
+  let ()= debug_lire_choices capacities in
+  let ()= debug_lire_peoples preferences in
+  let graph = build_graph preferences capacities in
+  let sgraph = gmap graph (fun x -> (string_of_int x)) in
+  let () = export outfile2 sgraph in
+  let (fl2,fgraph)= construction_gap_solution graph 0 1 0 in
+  let graph = construct_flow_solution graph fgraph in
+  let list_peoples = results_to_list_peoples graph preferences capacities in
+  let list_sport = results_to_list_sport graph preferences capacities in
   let graph = gmap graph (fun x -> (string_of_int x)) in
-  let fgraph = gmap fgraph (fun x -> (string_of_int x)) in
-  let () =export outfile fgraph in 
-  let () =export final graph in 
-  Printf.printf "Le flot max = %d\n" fl ; ()
+  let () = export_all_to_txt outfile list_peoples list_sport in
+  let () = export final graph in
+  Printf.printf "Le flot max = %d\n" fl2 ; ()
